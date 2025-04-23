e678c351a7a91295d97463b93148e4e47f57d8ab
Vitor Lima Silva <110677656+Vitoroturan2905@users.noreply.github.com>
2025-04-23 11:23:25 +0000

    upando o codigo iniciante do xadrez

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,34 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+  
+    int casa_bispo = 0;
+    int bispo = 5;
+    int torre = 5;
+    int rainha = 8;
+    int casa_torre = 0;
+    int casa_rainha = 0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+     //Movimento do bispo
+     while (casa_bispo < bispo)
+     {
+         printf("BISPO: CIMA DIREITA \n");
+         casa_bispo++;
+     }
+ 
+     //Movimento da torre
+     do{
+         printf("TORRE: DIREITA \n");
+         casa_torre++;
+     } while (casa_torre < torre);
+ 
+ 
+     //Movimento da rainha
+     for (int casa_rainha = 0; casa_rainha < rainha; casa_rainha++);
+     {
+         printf("RAINHA: ESQUERDA \n");
+         casa_rainha++;
+     }
 
     return 0;
 }
